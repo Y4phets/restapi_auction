c6edbd5aa14beecad2d95f2a2a40157b7ff95159
Dmitriy <you@example.com>
2020-12-21 15:42:58 +0300

    Add time zone

--- django_auction/settings.py
+++ django_auction/settings.py
@@ -109,13 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
+USE_TZ = True
 
 USE_I18N = True
 
 USE_L10N = True
-
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
